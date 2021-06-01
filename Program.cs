a98c6f4f6b155f25ee7fe9c2de73f0ea3d5f8028
Bryson Goins <brysonmg@gmail.com>
2021-06-01 15:28:12 -0500

    bugs fixed, ready to play!

--- Program.cs
+++ Program.cs
@@ -1,19 +1,138 @@
 ﻿using System;
+using System.Threading;
 
 int PlayerPoints = 0;
 int CompPoints = 0;
+int PlayTo = 0;
 
-// void main()
-// {
-
-// };
-
-void ScoreCard(int PlayerScore, int CompScore)
+void GetScore()
 {
+    string TopScore = string.Empty;
+    TopScore = string.Empty;
+    while (TopScore == string.Empty)
+    {
+        Console.WriteLine("What score do you want to play to?");
+        TopScore = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(TopScore))
+        {
+            Console.WriteLine("Fine, I didn't want to play anyway...");
+            Environment.Exit(0);
+        }
+
+        foreach (char x in TopScore)
+        {
+            if (!char.IsDigit(x))
+            {
+                Console.Clear();
+                Console.WriteLine("Please type a number...");
+                TopScore = string.Empty;
+                break;
+            };
+        };
+
+        if (TopScore == string.Empty)
+        {
+            continue;
+        }
+
+        if (int.Parse(TopScore) == 0)
+        {
+            Console.Clear();
+            Console.WriteLine("Nice Try... Pick a number ABOVE 0...");
+            TopScore = string.Empty;
+            continue;
+        }
+
+
+        if (int.Parse(TopScore) >= 100)
+        {
+            Console.Clear();
+            Console.WriteLine("Wait, you want to play HOW MANY rounds?");
+            Thread.Sleep(1500);
+            Console.WriteLine(".");
+            Thread.Sleep(500);
+            Console.WriteLine("..");
+            Thread.Sleep(500);
+            Console.WriteLine("...");
+            Thread.Sleep(500);
+            Console.WriteLine("Nope. I don't think so... Pick a lower number.");
+            Thread.Sleep(2000);
+            Console.Clear();
+            TopScore = string.Empty;
+        }
+    }
+
+    PlayTo = int.Parse(TopScore);
+};
+
+void main()
+{
+    ScoreCard(PlayerPoints, CompPoints, PlayTo);
+    Play();
+};
+
+void runGame()
+{
+    PlayerPoints = 0;
+    CompPoints = 0;
+    Console.Clear();
+    GetScore();
+    main();
+}
+runGame();
+
+void ScoreCard(int PlayerScore, int CompScore, int EndScore)
+{
+    if (PlayerScore == EndScore)
+    {
+        Console.WriteLine($@"
+        ------------------------------
+        |  Player: {PlayerScore}  |  Computer: {CompScore} |
+        ------------------------------
+        You Won The Game!
+        ");
+        Thread.Sleep(1500);
+        Console.Write("Would you like to play again? (Y/N): ");
+        string Resp = Console.ReadLine().ToLower();
+        if (Resp == "y")
+        {
+            PlayerPoints = 0;
+            CompPoints = 0;
+            runGame();
+        }
+        else
+        {
+            Environment.Exit(0);
+        }
+    }
+    else if (CompScore == EndScore)
+    {
+        Console.WriteLine($@"
+        ------------------------------
+        |  Player: {PlayerScore}  |  Computer: {CompScore} |
+        ------------------------------
+        You Lost The Game!
+        ");
+        Thread.Sleep(1500);
+        Console.Write("Would you like to play again? (Y/N): ");
+        string Resp = Console.ReadLine().ToLower();
+        if (Resp == "y")
+        {
+            runGame();
+        }
+        else
+        {
+            Environment.Exit(0);
+        }
+    };
+
     Console.WriteLine($@"
+    First to {EndScore} wins.
     ------------------------------
-    | Player: {PlayerScore}   |  Computer: {CompScore} |
+    |  Player: {PlayerScore}  |  Computer: {CompScore} |
     ------------------------------
+    Press Enter to quit.
     ");
 };
 
@@ -25,8 +144,23 @@ void Play()
     3) Scissors
     ");
 
-    int Choice = int.Parse(Console.ReadLine());
+    string Input = Console.ReadLine();
 
+    if (string.IsNullOrWhiteSpace(Input))
+    {
+        Console.WriteLine("Thanks for playing!");
+        Environment.Exit(0);
+    };
+
+    if (Input != "1" && Input != "2" && Input != "3")
+    {
+        Console.Clear();
+        Console.WriteLine("Please make a valid selection...");
+        main();
+    };
+
+    int Choice = int.Parse(Input);
+    Console.Clear();
     if (Choice == 1)
     {
         UseRock();
@@ -65,11 +199,56 @@ void Play()
         UseScissors();
     };
 
-    if (Choice == CompChoice)
-    {
-        Console.WriteLine("----------DRAW----------");
+    Thread.Sleep(2500);
 
+    if (Choice == CompChoice)
+    {//Result: Draw
+        Console.Clear();
+        Console.WriteLine("You Drew That Round");
+        main();
     }
+    else if (Choice == 1 && CompChoice == 2)
+    {//Result: Computer Wins
+        Console.Clear();
+        Console.WriteLine("You Lost That Round");
+        CompPoints++;
+        main();
+    }
+    else if (Choice == 1 && CompChoice == 3)
+    {//Result: Player Wins
+        Console.Clear();
+        Console.WriteLine("You Won That Round");
+        PlayerPoints++;
+        main();
+    }
+    else if (Choice == 2 && CompChoice == 1)
+    {//Result: Player Wins
+        Console.Clear();
+        Console.WriteLine("You Won That Round");
+        PlayerPoints++;
+        main();
+    }
+    else if (Choice == 2 && CompChoice == 3)
+    {//Result: Computer Wins
+        Console.Clear();
+        Console.WriteLine("You Lost That Round");
+        CompPoints++;
+        main();
+    }
+    else if (Choice == 3 && CompChoice == 1)
+    {//Result: Computer Wins
+        Console.Clear();
+        Console.WriteLine("You Lost That Round");
+        CompPoints++;
+        main();
+    }
+    else if (Choice == 3 && CompChoice == 2)
+    {//Result: Player Wins
+        Console.Clear();
+        Console.WriteLine("You Won That Round");
+        PlayerPoints++;
+        main();
+    };
 
 
 };
